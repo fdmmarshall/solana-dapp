3ae719429380c2dbd7d69704c34d91260065e5e6
Flor Marshall <flordmarshall@gmail.com>
2021-11-26 21:55:53 -0500

    edit for deployment

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("969LoebfA2i4DsrPXqUdHEv2aXd9XWrJgkcwgoPbVitE");
 
 #[program]
 pub mod myepicproject {
