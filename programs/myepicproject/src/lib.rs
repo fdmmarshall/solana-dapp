bad04fe4d7f89ad281489e6f620f83baaceb04d2
Flor Marshall <flordmarshall@gmail.com>
2021-11-26 21:29:57 -0500

    remove extra curly bracket

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -6,15 +6,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
-    // Get a reference to the account.
     let base_account = &mut ctx.accounts.base_account;
-    // Initialize total_gifs.
     base_account.total_gifs = 0;
     Ok(())
   }
-}
-
-pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+  
+	// Another function woo!
+  pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
     // Get a reference to the account and increment total_gifs.
     let base_account = &mut ctx.accounts.base_account;
     base_account.total_gifs += 1;
@@ -22,14 +20,13 @@ pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
   }
 }
 
-// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
 pub struct StartStuffOff<'info> {
-    #[account(init, payer = user, space = 9000)]
-    pub base_account: Account<'info, BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
 }
 
 // Specify what data you want in the AddGif Context.
@@ -40,7 +37,6 @@ pub struct AddGif<'info> {
   pub base_account: Account<'info, BaseAccount>,
 }
 
-// Tell Solana what we want to store on this account.
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
